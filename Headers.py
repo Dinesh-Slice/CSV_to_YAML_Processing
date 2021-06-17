32562114666e88d67e7fe4a1f81e0a507736d06a
Dinesh-Slice <83824052+Dinesh-Slice@users.noreply.github.com>
2021-06-17 09:47:42 -0400

    Added Comments

--- Headers.py
+++ Headers.py
@@ -5,11 +5,13 @@ question = "Question"
 group = "Disp. Group"
 order = "Order"
 
+
+# Write headers that are useless to the yaml output
+to_be_dropped = ["Wave", "Notes / Questions / Decision", "Card", "Screen"]
+
+# Change Dict Key based on the grouping column
 grouping_enum = {
     "ClassRelated": "classSpecific",
     "General": "generic",
     "ClaimHistory": "priorClaims",
 }
-
-# Write headers that are useless to the yaml output
-to_be_dropped = ["Wave", "Notes / Questions / Decision", "Card", "Screen"]
